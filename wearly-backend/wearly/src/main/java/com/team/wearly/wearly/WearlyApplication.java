00a91241761595ca06988bff5afbe874c5f79cdd
허준형 <gjwnsgud38@gmail.com>
2026-01-08 17:13:53 +0900

    Feat : 의존성 추가 JWT 등 의존성 추가

---
+++ wearly-backend/wearly/src/main/java/com/team/wearly/wearly/WearlyApplication.java
@@ -0,0 +1,13 @@
+package com.team.wearly.wearly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearlyApplication.class, args);
+	}
+
+}
---
+++ wearly-backend/wearly/src/test/java/com/team/wearly/wearly/WearlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.wearly.wearly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

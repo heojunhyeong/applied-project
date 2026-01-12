7341e3ecccf808a57bc6910917e2a106e787a83f
2000JCH <rkwkal59@naver.com>
2026-01-12 10:35:34 +0900

    Docker, nginx 완료

--- wearly-backend/wearly/src/main/java/com/team/wearly/domain/order/service/OrderService.java
+++ wearly-backend/wearly/src/main/java/com/team/wearly/domain/order/service/OrderService.java
@@ -1,20 +1,4 @@
 package com.team.wearly.domain.order.service;
 
-import com.team.wearly.domain.order.dto.CartRequestDto;
-import com.team.wearly.domain.order.dto.CartResponseDto;
-
-import java.util.List;
-
 public interface OrderService {
-    // 장바구니 조회
-    List<CartResponseDto> getCartItems(Long userId);
-
-    // 장바구니 상품 추가
-    CartResponseDto addCart(Long userId, CartRequestDto requestDto);
-
-    // 장바구니 상품 삭제
-    void deleteCartItem(Long userId, Long productId);
-
-    // 장바구니 전체 비우기
-    void clearCart(Long userId);
 }
--- wearly-backend/wearly/src/main/java/com/team/wearly/domain/user/service/UserServiceImpl.java
+++ wearly-backend/wearly/src/main/java/com/team/wearly/domain/user/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
-package com.team.wearly.domain.user.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
+//package com.team.wearly.domain.user.service;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserServiceImpl implements UserService {
+//}

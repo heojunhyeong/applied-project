5aa8e16a2e2cdaf6aecd1ba1cdad15d6a2eecf58
2000JCH <rkwkal59@naver.com>
2026-01-19 10:09:41 +0900

    SecurityConfig 서버 주소 추가

--- wearly-backend/wearly/src/main/java/com/team/wearly/global/config/SecurityConfig.java
+++ wearly-backend/wearly/src/main/java/com/team/wearly/global/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:70"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:70", "http://3.35.27.165:70"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
